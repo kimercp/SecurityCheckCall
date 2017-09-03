8970395475e043863cb1dff8fd0b245d76966048
WyrwiGnatus <30984606+kimercp@users.noreply.github.com>
2017-09-03 21:05:28 +0100

    added menu with exit option

--- app/src/main/java/com/example/workstation/securitycheckcall/MainActivity.java
+++ app/src/main/java/com/example/workstation/securitycheckcall/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.workstation.securitycheckcall;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+/* Main menu when user may start the alarm, add new one or delete. */
 public class MainActivity extends AppCompatActivity {
 
     boolean isAllCaps;
@@ -16,20 +23,70 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    // this method will open new activity, where user may choose time, occurrence, etc.
+    // activate the menu on this activity
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        // close the app option "exit" from menu
+        if (id == R.id.action_exit) {
+            // create a dialog (question to user to confirm exit)
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            // Setting Dialog Message
+            builder.setMessage(R.string.dialog_conifrmExit)
+                    // Setting Dialog Title
+                    .setTitle(R.string.action_exit)
+                    // Setting Icon to Dialog
+                    .setIcon(null) // or .setIcon(R.drawable.icon_name) to display the icon
+                    // Setting Positive "Yes" Btn
+                    .setPositiveButton(R.string.dialog_Yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // Write your code here to execute after yes
+                            System.exit(0);
+                        }
+                    })
+                    // Setting Negative "Cancel" Btn
+                    .setNegativeButton(R.string.dialog_Cancel, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // User cancelled the dialog
+                            Toast.makeText(getApplicationContext(),
+                                    "Ok, enjoy the app", Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    })
+                    // Showing Alert Dialog
+                    .show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    /* This method will open new activity after
+     clicking on button "New alarm"
+     where user may choose time, occurrence, etc.*/
     public void newAlarm(View view) {
         Intent intent = new Intent(this, NewAlarmActivity.class);
         startActivity(intent);
     }
 
+
     // test method to change letters for all capitals
-    public void testButton(){
-        TextView textView = (TextView)findViewById(R.id.txtCreatedByKimercp);
+    public void testButton(View view) {
+        TextView textView = (TextView) findViewById(R.id.txtCreatedByKimercp);
         if (isAllCaps) {
             textView.setAllCaps(false);
             isAllCaps = false;
-        }
-        else {
+        } else {
             textView.setAllCaps(true);
             isAllCaps = true;
         }
--- app/src/main/java/com/example/workstation/securitycheckcall/NewAlarmActivity.java
+++ app/src/main/java/com/example/workstation/securitycheckcall/NewAlarmActivity.java
@@ -2,12 +2,26 @@ package com.example.workstation.securitycheckcall;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
+/* This activity will serve adding new alarms only
+* User may set the name, occurrence and time when
+* the alarm supposed to start. */
 public class NewAlarmActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_alarm);
+    }
+
+    /* This method will save a new alarm after
+      clicking on button "Save New Alarm"      */
+    public void saveAlarm_buttonOnClick(){
+        EditText edtAlarmName = (EditText)findViewById(R.id.edtAlarmName);
+        EditText edtOccurrence = (EditText)findViewById(R.id.edtOccurrence);
+        TimePicker tmpAlarmStart = (TimePicker)findViewById(R.id.tmpAlarmStart);
+
     }
 }
